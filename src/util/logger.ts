2d5f605adc45909fa0c81761b8490916ea592daa
Wolfgang Zolleis <wzolleis@web.de>
2018-10-14 21:07:03 +0200

    dependency updates

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,15 +1,26 @@
-import winston, { Logger } from "winston";
+import winston from "winston";
 
-const logger = new (Logger)({
+const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.json(),
     transports: [
-        new (winston.transports.Console)({level: process.env.NODE_ENV === "production" ? "error" : "debug"}),
-        new (winston.transports.File)({filename: "debug.log", level: "debug"})
+        //
+        // - Write to all logs with level `info` and below to `combined.log`
+        // - Write all logs error (and below) to `error.log`.
+        //
+        new winston.transports.File({filename: "error.log", level: "error"}),
+        new winston.transports.File({filename: "combined.log"})
     ]
 });
 
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
 if (process.env.NODE_ENV !== "production") {
-    logger.debug("Logging initialized at debug level");
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple()
+    }));
 }
-
 export default logger;
 
